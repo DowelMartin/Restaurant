e4c2acce63d25acbd78f56cbb6d76f4293c1a9d8
LipaMar <48134943+LipaMar@users.noreply.github.com>
2020-01-21 18:40:44 +0100

    Update CheckOrdersPage.cs

--- Restaurant/Options/CheckOrdersPage.cs
+++ Restaurant/Options/CheckOrdersPage.cs
@@ -11,8 +11,26 @@ namespace Restaurant.Options
         public CheckOrdersPage() { }
         public CheckOrdersPage(string name) => this.Name = name;
         public override void Execute()
-        { 
-       
+        {
+            int pick = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Moje zamówienia:");
+                // wypisz zamówienia
+                Console.WriteLine("Wybierz zamówienie, które chcesz anulować lub wprowadź \'X\', żeby powrócić do menu");
+                string input = Console.ReadLine();
+                if (input.ToUpper() == "X") return;
+                try
+                {
+                    pick = int.Parse(input);
+                }
+                catch (Exception) { }
+
+                //anuluj zamówienie
+            } while (pick <= 0);
+
+
         }
     }
 }
